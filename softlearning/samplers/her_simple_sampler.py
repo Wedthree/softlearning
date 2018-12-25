c9c432735e48119a61ba8a611558886b8a3750b7
mikechzhou <mikechzhou@tencent.com>
2018-12-25 19:07:58 +0800

    te

--- softlearning/samplers/her_simple_sampler.py
+++ softlearning/samplers/her_simple_sampler.py
@@ -1,11 +1,26 @@
 import numpy as np
+import math
 
 from .sampler_base import BaseSampler
 
 
 class HerSimpleSampler(BaseSampler):
-    def __init__(self, **kwargs):
+    def __init__(self, replay_k=99, **kwargs):
         super(HerSimpleSampler, self).__init__(**kwargs)
+
+        """ Sampler that can be used for HER experience replay.
+        Args:
+            replay_k (int): the ratio between HER replays and regular replays (e.g. k = 4 -> 4 times
+                as many HER replays as regular replays are used)
+            reward_fun (function): function to re-compute the reward with substituted goals
+        """
+
+        self._future_p = 1 - (1. / (1 + replay_k))
+
+        def reward_fun(ag_2, g, info):  # vectorized
+            return self.env.compute_reward(achieved_goal=ag_2, desired_goal=g, info=info)
+
+        self._reward_fun = reward_fun
 
         self._path_length = 0
         self._path_return = 0
@@ -44,9 +59,7 @@ class HerSimpleSampler(BaseSampler):
                 observation_keys=getattr(self.env, 'observation_keys', None))
             last_path.update({'infos': self._infos})
 
-            for key, values in last_path.items():
-                print(key, values.shape)
-
+            self.add_her_samples(last_path, self._path_length)
 
 
             self._last_n_paths.appendleft(last_path)
@@ -68,6 +81,23 @@ class HerSimpleSampler(BaseSampler):
             self._current_observation = next_observation
 
         return self._current_observation, reward, terminal, info
+
+
+    def add_her_samples(self, path, path_length):
+        print(self._future_p)
+        print(self._reward_fun)
+        print(last_path_length)
+        her_sample_size = math.ceil(self._future_p*path_length)
+        t_samples =  np.random.randint(last_path_length, size=her_sample_size)
+
+        transitions = {key: path[key][t_samples].copy()
+                               for key in path.keys()}
+
+        print(transitions)
+
+
+
+
 
     def random_batch(self, batch_size=None, **kwargs):
         batch_size = batch_size or self._batch_size
